97ac061fc83ee401900670c4a3d0b1f6ed9bf375
ebrault <emmet.brault@gmail.com>
2018-10-18 17:07:03 -0400

    Dummy commit

---
+++ assets/scripts/film/ui.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const addFilmSuccess = function () {
+  $('#display-films').html('')
+  $('#display-message').html('Add successful!')
+  $('#display-message').css('color', 'green')
+  $('#add-film-form').trigger('reset')
+}
+
+const addFilmFailure = function () {
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#add-film-form').trigger('reset')
+}
+
+const getAllFilmsSuccess = function (data) {
+  $('#display-message').html('')
+  $('#display-films').html('')
+  data.films.forEach(function (film) {
+    const filmHtml = (`
+        <h4>Title: ${film.title}</h4>
+        <p>Director: ${film.director}</p>
+        <p>ID: ${film.id}</p>
+        </ br>
+      `)
+    $('#display-films').append(filmHtml)
+  })
+}
+
+const getAllFilmsFailure = function () {
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+}
+
+const getOneFilmSuccess = function (data) {
+  $('#display-message').html('')
+  $('#display-films').html('')
+  const film = data.film
+  const filmHtml = (`
+      <h4>Title: ${film.title}</h4>
+      <p>Director: ${film.director}</p>
+      <p>ID: ${film.id}</p>
+      </ br>
+    `)
+  $('#display-films').html(filmHtml)
+  $('#get-one-film-form').trigger('reset')
+}
+
+const getOneFilmFailure = function () {
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#get-one-film-form').trigger('reset')
+}
+
+const modifyFilmSuccess = function (data) {
+  $('#display-message').html('')
+  $('#display-films').html('')
+  const film = data.film
+  const filmHtml = (`
+    <h4>Title: ${film.title}</h4>
+    <p>Director: ${film.director}</p>
+    <p>ID: ${film.id}</p>
+    `)
+  $('#display-films').append(filmHtml)
+  $('#modify-film-form').trigger('reset')
+}
+
+const modifyFilmFailure = function () {
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#modify-film-form').trigger('reset')
+}
+
+const deleteFilmSuccess = function () {
+  $('#display-message').html('Deletion successful!')
+  $('#display-message').css('color', 'green')
+  $('#delete-film-form').trigger('reset')
+}
+
+const deleteFilmFailure = function () {
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#delete-film-form').trigger('reset')
+}
+
+module.exports = {
+  addFilmSuccess,
+  addFilmFailure,
+  getAllFilmsSuccess,
+  getAllFilmsFailure,
+  getOneFilmSuccess,
+  getOneFilmFailure,
+  modifyFilmSuccess,
+  modifyFilmFailure,
+  deleteFilmSuccess,
+  deleteFilmFailure
+}
