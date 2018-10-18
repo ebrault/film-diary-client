5f2b6f653c5233f0f984082031afbde046f59440
ebrault <emmet.brault@gmail.com>
2018-10-18 10:54:10 -0400

    Added curl scripts to test client auth
    - Wrote four curl scripts for client user auth based on routes in
    API template.
    - Sign Up, Sign In, Change Password, and Sign Out

---
+++ curl-scripts/change-password.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+curl "http://localhost:4741/change-password" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLD}"'",
+      "new": "'"${NEW}"'"
+    }
+  }'
+
+echo
