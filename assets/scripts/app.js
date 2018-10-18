6256a53308c903284e0f0899f294695056ca9760
ebrault <emmet.brault@gmail.com>
2018-10-18 15:28:55 -0400

    Added user auth, film forms to client
    Added user auth and film forms to HTML using getFormFields
    Added styling to hide required forms
    Added event handlers to app.js
    Added user events to user/events.js
    Added user api calls to user/api.js
    Added user ui response to user/ui.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,17 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const userEvents = require('./user/events')
+// const filmEvents = require('./film/events')
+
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', userEvents.onSignUp)
+  $('#sign-in-form').on('submit', userEvents.onSignIn)
+  $('#change-password-form').on('submit', userEvents.onChangePassword)
+  $('#sign-out-button').on('click', userEvents.onSignOut)
+  // $('#add-film-form').on('submit', filmEvents.onAddFilm)
+  // $('#modify-film-form').on('submit', filmEvents.onModifyFilm)
+  // $('#get-all-films-button').on('click', filmEvents.onGetAllFilms)
+  // $('#get-one-film-form').on('submit', filmEvents.onGetOneFilm)
+  // $('#delete-film-form').on('submit', filmEvents.onDeleteFilm)
 })
---
+++ assets/scripts/user/ui.js
@@ -0,0 +1,90 @@
+'use strict'
+
+const store = require('../store')
+
+const signUpSuccess = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Sign up successful!')
+  $('#display-message').css('color', 'green')
+  $('#sign-up-form').trigger('reset')
+}
+
+const signUpFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#sign-up-form').trigger('reset')
+}
+
+const signInSuccess = function (response) {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Sign in successful!')
+  $('#display-message').css('color', 'green')
+  $('#sign-in-form').trigger('reset')
+  store.user = response.user
+  $('#sign-up-form').addClass('hidden')
+  $('#sign-in-form').addClass('hidden')
+  $('#change-password-form').removeClass('hidden')
+  $('#sign-out-button').removeClass('hidden')
+  $('#add-film-form').removeClass('hidden')
+  $('#get-all-films-button').removeClass('hidden')
+  $('#get-one-film-form').removeClass('hidden')
+  $('#modify-film-form').removeClass('hidden')
+  $('#delete-film-form').removeClass('hidden')
+}
+
+const signInFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#sign-in-form').trigger('reset')
+}
+
+const changePasswordSuccess = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Password changed successfully!')
+  $('#display-message').css('color', 'green')
+  $('#change-password-form').trigger('reset')
+}
+
+const changePasswordFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#change-password-form').trigger('reset')
+}
+
+const signOutSuccess = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Sign out successful!')
+  $('#display-message').css('color', 'green')
+  $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').trigger('reset')
+  $('#change-password-form').trigger('reset')
+  $('#sign-up-form').removeClass('hidden')
+  $('#sign-in-form').removeClass('hidden')
+  $('#change-password-form').addClass('hidden')
+  $('#add-film-form').addClass('hidden')
+  $('#get-all-films-button').addClass('hidden')
+  $('#get-one-film-form').addClass('hidden')
+  $('#modify-film-form').addClass('hidden')
+  $('#delete-film-form').addClass('hidden')
+  $('#sign-out-button').addClass('hidden')
+}
+
+const signOutFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
